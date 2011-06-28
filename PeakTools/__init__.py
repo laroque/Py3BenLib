7121ce8bafc9f49126f841fc73d8da412be06298
Ben LaRoque <benjamin.laroque@gmail.com>
2011-06-28 09:32:20 -0700

    Restructured, creating a subdirectory to hold all peak fitting and attribute determination tools.

---
+++ PeakTools/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''
+  File to make this directory importable as a package.
+
+  All "fit-like" functions will live here. This will include histogram interval fitting to predefined functions, area determinations, width determinations, etc.
+'''
+from .LorentzianFit import LorentzianFit
--- __init__.py
+++ __init__.py
@@ -4,4 +4,4 @@ File to make this a package.
 '''
 
 from .Mag3to2 import Mag3to2
-from .PeakFit import LorentzianFit
+from .PeakTools import LorentzianFit
